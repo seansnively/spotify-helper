1d7ca6e58b9747b7ea972dba4873765634a4b817
Sean Snively <gator123ss@gmail.com>
2024-10-04 23:12:06 -0400

    making top track button for getting data from aws

--- spotify-helper-react-app/src/components/TopTracks.jsx
+++ spotify-helper-react-app/src/components/TopTracks.jsx
@@ -1,12 +1,30 @@
 import { UserContext } from '../context/StateProvider';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 const TopTracks = () => {
     const { user} = useContext(UserContext);
+    
 
+    const [data, setData] = useState(null);
+    let timeRange = 'short_term';
+    let songLimit = '5';
+
+    const fetchData = async () => {
+        try {
+          const response = await fetch(`${import.meta.env.VITE_TEST_VAR}?userAuthToken=${user.authToken}&timeRange=${timeRange}&songLimit=${songLimit}`);
+    
+          const songsWithInsightsResponseBody = await response.json();
+          setData(songsWithInsightsResponseBody);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+    };
     return (
         <div>
-            <h1>Hello, {user.authToken}!</h1>
+            <button onClick={fetchData}>Fetch Data</button>
+            {data && (
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+            )}
         </div>
     );
 };
